3f7f1088bab56cb0fd29198c89e4bbb1d790b1c7
fxx <394967886@qq.com>
2023-02-04 21:31:58 +1300

    change2python

---
+++ test1.py
@@ -0,0 +1,12 @@
+class Product:
+    def __int__(self,name:str,price:float,spec_num:int)->None:
+        self.name =name
+        self.price = price
+        self.spec_num = spec_num
+
+class Promotion:
+    def __int__(self):
+        pass
+
+    @property
+
