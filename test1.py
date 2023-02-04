2e770cf411ac013d9d840fe50d4ec75e5c2ea471
fxx <394967886@qq.com>
2023-02-04 22:27:55 +1300

    main_test_seperate

--- test1.py
+++ test1.py
@@ -9,5 +9,6 @@ class Promotion:
         pass
     
     def sayHellp(self):
-        print("I am fxx")
-            
+        print("I am fxx!!!")
+
+print("test_saperatetest_saperatetest_saperatetest_saperate")
