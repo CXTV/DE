8291e4315faaab78c504b9db8ecf85b5ef436986
fxx <394967886@qq.com>
2023-02-08 21:21:51 +1300

    factoty_pattern

---
+++ factory_pattern.py
@@ -0,0 +1,39 @@
+from abc import ABC,abstractmethod
+
+
+class Payment(ABC):   
+    @abstractmethod
+    def pay(self):
+        pass
+
+class AliPay(Payment):
+    def pay(self):
+        print("Paying by Ali...")
+
+class PaypalPay(Payment):
+    def pay(self):
+        print("Pay by Paypal...")
+
+#版本一：普通工厂
+class OnlinePay_LOW:
+    def creat_payment(self,method):
+        if method =='alipay':
+            return AliPay()
+        elif method =='paypal':
+            return PaypalPay()
+        else:
+            print("NO SUCH PAY METHOD!")
+
+#版本二:依赖注入
+class OnlinePay:
+    def __init__(self,pay_method:Payment) -> None:
+        self._pay_method = pay_method
+
+    def pay_online(self):
+        self._pay_method.pay()
+        return
+    
+alipay =AliPay()
+pay1 = OnlinePay(alipay)
+pay1.pay_online()
+
