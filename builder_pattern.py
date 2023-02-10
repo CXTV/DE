396298376acdc0575907fae4a882a042a9441ea7
fxx <394967886@qq.com>
2023-02-10 13:02:59 +1300

    builder_pattern

--- builder_pattern.py
+++ builder_pattern.py
@@ -1,13 +1,20 @@
 from dataclasses import dataclass
 from abc import ABC,abstractmethod
 
-#产品
+#产品1
 @dataclass
 class Player:
     face:str = None
     hair:str = None
     body:str = None
-    
+
+#产品2
+@dataclass
+class Player2:
+    face:str = None
+    hair:str = None
+    body:str = None
+
 #抽象建造者
 class Builder(ABC):
     
@@ -23,7 +30,7 @@ class Builder(ABC):
     def build_body(self):
         pass
 
-#具体建造者
+#具体建造者1
 class SexyGirl(Builder):
     
     def __init__(self) -> None:
@@ -31,14 +38,17 @@ class SexyGirl(Builder):
 
     def build_face(self):
         self.player.face = "baby face"
-    
+        return self 
+     
     def build_hair(self):
-        self.player.hair = "brown body"     
-
+        self.player.hair = "brown hair"     
+        return self 
+    
     def build_body(self):
         self.player.body = "hot body"
-
-#具体建造折
+        return self 
+    
+#具体建造着2
 class Monster(Builder):
     
     def __init__(self) -> None:
@@ -46,22 +56,30 @@ class Monster(Builder):
 
     def build_face(self):
         self.player.body = "Monster face"
+        return self 
     
     def build_hair(self):
         self.player.hair = "Monster hair"     
-
+        return self ## 返回自己可以类似于java的链式调用
+    
     def build_body(self):
         self.player.body = "Monster body"
-
+        return self
+    
 #指挥者
 class BuildDirector: ##控制顺序
-    def build_charecher(self,builder:Builder):
-        builder.build_hair()
-        builder.build_face()
-        builder.build_body()
-        return builder.player
 
+    def build_charecher1(self,builder:Builder):
+        builder.build_face().build_body().build_hair()
+        return builder.player
+    
+    def build_charecher2(self,builder:Builder):
+        builder.build_body().build_hair().build_face()
+        return builder.player
+    
 sexy_girl = SexyGirl()
 director = BuildDirector()
-p = director.build_charecher(sexy_girl)
-print(vars(p)) #查看所有类属性的所有值！
+p = director.build_charecher2(sexy_girl)
+print(vars(p)) #打印出p的所有方法
+print(isinstance(p,Player)) #判断实例是否属于某个实例1
+print(type(p) is Builder) #判断实例是否属于某个实例2
