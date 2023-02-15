e694d9cd117d0cca7850d297f4644683a8b12b77
fxx <394967886@qq.com>
2023-02-15 16:59:16 +1300

    review_design_pattern

--- abstract_factory2.py
+++ abstract_factory2.py
@@ -1,13 +1,52 @@
-import re
+from abc import ABC,abstractmethod
 
-string = "dfdf('':46%%^)"
+class Payment:
+    @abstractmethod
+    def pay(self):
+        pass
 
-# 使用正则表达式提取数字
-numbers = re.findall(r'(\d+)', string)
+class Alipay(Payment):
+    def pay(self):
+        print("paying by ali")
+        return
 
-# 转换为整数类型
-numbers = [int(number) for number in numbers]
+class Paypal(Payment):
+    def pay(self):
+        print("paying by paypal...")
 
-# 访问第一个数字
-number = numbers[0]
-print(number)
+class Bank:
+    def cost(self):
+        print("paying by bank....")
+
+class NewBankPay(Payment,Bank):
+    def pay(self):
+        self.cost()
+
+
+class Check_payment:
+    def __init__(self,payment:Payment) -> None:
+        self.__payment = payment
+        self.pay_flag = 1
+        self.check_pay()
+
+    def __call__(self, ) -> None:
+        print("I am __call__")
+        return 
+
+    def check_pay(self):
+        print("checking the payment is right or not...")
+        if self.pay_flag:
+            print("PAYING APROVED..WATING PAYING...")
+            self.__payment.pay()
+        else:
+            print("Something wrong with the payment.")
+
+ali_pay = Alipay()
+
+new_bank_pay = NewBankPay()
+
+Check_payment(new_bank_pay)
+
+
+
+
